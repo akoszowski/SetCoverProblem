b3382ce9c2a9e5a12b49e9c6720aa0af62af72dc
akoszowski <a.koszowski@wp.pl>
2020-05-04 13:22:12 +0200

    Output - spaces

--- cover/Precise.java
+++ cover/Precise.java
@@ -14,35 +14,50 @@ public class Precise extends Algorithm {
         int minNeeded = setsNum + 1;
         boolean[] covered = new boolean[instanceBound + 1];
         ArrayList<Set> familySubset = new ArrayList<>();
+        ArrayList<Integer> subsetIds = new ArrayList<>();
 
         long curArr = 1, arrNum = (1 << setsNum), curVal;
         while (curArr < arrNum) {
             familySubset.clear();
+            subsetIds.clear();
 
             curVal = curArr;
             for (int i = 0; curVal > 0; ++i) {
                 if (curVal % 2 == 1) {
                     familySubset.add(setsFamily.get(i));
+                    subsetIds.add(i);
                 }
 
                 curVal /= 2;
             }
 
-            if (isBetter(familySubset) && checkIfCovers(familySubset, instanceBound)) {
+            if (isBetter(subsetIds, minNeeded) && checkIfCovers(familySubset, instanceBound)) {
                 minNeeded = familySubset.size();
                 System.err.print(minNeeded + "\n");
                 solution.clear();
 
-                curVal = curArr;
-                for (int i = 0; curVal > 0; ++i) {
-                    if (curVal % 2 == 1) {
-                        solution.add(i + 1);
-                    }
-                    curVal /= 2;
-                }
+                solution.addAll(subsetIds);
             }
 
             curArr++;
         }
+    }
+
+    private boolean isBetter(ArrayList<Integer> subsetIds, int minNeeded) {
+        if (subsetIds.size() < minNeeded) {
+            return true;
+        }
+        else if (subsetIds.size() == minNeeded) {
+            for (int i = 0; i < subsetIds.size(); ++i) {
+                if (subsetIds.get(i) > solution.get(i)) {
+                    return false;
+                }
+                else if (subsetIds.get(i) < solution.get(i)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
